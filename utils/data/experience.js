8a4364c49f818d12b0b13f1fcfa332aa0a764412
Laharimadishetty <laharifye@gmail.com>
2024-10-10 21:11:16 -0400

    projects section updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -6,12 +6,13 @@ export const experiences = [
     duration: "(January 2024 - Present)",
     description: [
       "Implemented a user study framework utilizing OpenAI's API and ChatGPT, where 63 users were asked to assume a specific personality trait from the Big Five and a predetermined age, engaging in 100 role assumption scenarios.",
-      "Introduced 12 visualization idioms across 3 different contexts for evaluation as part of the framework, providing nuanced insights into user interactions with various visualization styles. This included establishing and executing a structured workflow, analyzing 300 image descriptions and ratings, and offering valuable insights into user preferences across different visualization idioms."
+      "Introduced 12 visualization idioms across 3 different contexts for evaluation as part of the framework, providing nuanced insights into user interactions with various visualization styles. This included establishing and executing a structured workflow, analyzing 300 image descriptions and ratings, and offering valuable insights into user preferences across different visualization idioms.",
+      "Integrated visualization idioms with RESTful APIs, reducing analysis time by 50%, enhancing data insights."
     ]
     },
   {
     id: 2,
-    title: "Graduate Analyst",
+    title: "Software Development Engineer",
     company: "Barclays",
     duration: "(August 2022 - August 2023)",
     description: [
@@ -19,17 +20,19 @@ export const experiences = [
       "Optimized APIs, achieving a 50% reduction in network latency, enhancing the overall efficiency of the system.",
       "Employed a robust test-driven development approach using Cucumber for unit and acceptance tests, thereby strengthening the reliability of the project.",
       "Utilized continuous integration tools like Openshift and Jenkins, leading to a 30% decrease in deployment times.",
-      "Reduced operational times, leading to a faster response to customer inquiries and achieving a notable 45% enhancement in data retrieval speeds."
+      "Reduced operational times, leading to a faster response to customer inquiries and achieving a notable 45% enhancement in data retrieval speeds.",
+      "Enhanced digital banking applications with React, improving user experience for 2+ million users and increasing accessibility compliance by 30%",
+      "Managed API integration and deployment, improving performance by 35% & achieving 25% code reuse"
     ]
   },
   {
     id: 3,
-    title: "Mean Stack Developer Intern",
+    title: "Full Stack Developer Intern",
     company: "Biztime IT Solutions",
     duration: "(May 2020 - July 2020)",
     description: [
-      "Developed a Grievance Redressal Portal in MEAN stack, boosting incident reporting by 25%.",
-      "Implemented Worker Profile Management feature in the portal with secure authentication mechanisms using JSON Web Tokens (JWT)."
+      "Developed the Grievance Redressal Portal interface in React, boosting user experience and incident reporting by 25%.",
+      "Optimized backend in Node.js, enhancing data management and workflow efficiency for Worker Profile Management."
     ]
   }
 ]
